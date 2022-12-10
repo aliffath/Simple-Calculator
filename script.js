6187dcea4bdb6b8a9a5ed040e4af6766ab37dc24
Fatah <alifmiftakhulfatah@gmail.com>
2022-12-10 09:14:13 +0700

    validasi input data

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 let result = document.getElementById("inputtext");
 
 let calculate = (number) =>{
+    
     result.value += number;
 }
+console.log({result})
 
 let final = () =>{
     try{
 
-        result.value=eval(result.value)
+        result.value =eval(result.value)
     }
     catch(err){
         alert('Enter the valid input')
@@ -21,3 +23,11 @@ function clr(){
 function del(){
     result.value = result.value.slice(0,-1)
 }
+
+result.addEventListener("keypress", function(event) {
+    // Memeriksa apakah karakter yang ditekan adalah angka atau bukan
+    if (isNaN(event.key)) {
+    // Menghalangi karakter yang ditekan jika bukan angka
+    event.preventDefault();
+    }
+});
